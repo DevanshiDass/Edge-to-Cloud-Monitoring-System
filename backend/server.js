84115669f3c33fbd064f679b27829a94a38eb3b2
Devanshi Das <dasdevanshi7@gmail.com>
2026-02-15 13:29:59 +0530

    fix routes for render

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-require('./db');
-require('./mqtt');   // keep this
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth', require('./routes/auth'));
-app.use('/device', require('./routes/device'));
-app.use('/data', require('./routes/data'));
+// import routes
+const dataRoutes = require('./routes/data');
+const deviceRoutes = require('./routes/device');
+const authRoutes = require('./routes/auth');
+
+// use routes
+app.use('/data', dataRoutes);
+app.use('/device', deviceRoutes);
+app.use('/auth', authRoutes);
+
+// root test
+app.get('/', (req, res) => {
+    res.send("Edge to Cloud Monitoring API Running");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
+
+// start MQTT after server start
+require('./mqtt');
